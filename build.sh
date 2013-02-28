d8d788cc8f7293cb3f40b3ace2840de8c452cab6
Sergey Lukin <contact@sergeylukin.com>
2013-02-28 11:10:00 -0500

    Add exit in build script if tests fail

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 cd state/php
 composer install
-vendor/bin/phpunit example_car/car_test.php
-vendor/bin/phpunit example_delivery/delivery_test.php
-vendor/bin/phpunit example_turnstile/turnstile_test.php
+vendor/bin/phpunit example_car/car_test.php || { echo "car test failed"; exit 1; }
+vendor/bin/phpunit example_delivery/delivery_test.php || { echo "delivery test failed"; exit 1; }
+vendor/bin/phpunit example_turnstile/turnstile_test.php || { echo "turnstile test failed"; exit 1; }
 
-echo "Done running State pattern test suites"
+echo "State pattern test suite passed with success!"
